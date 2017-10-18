f9b2a9456091a036beb83524cd3eeb998de29c90
RaulAT <31300377+RaulAT@users.noreply.github.com>
2017-10-18 00:02:39 -0700

    Add files via upload

---
+++ CarpetaProyecto/TacosFranc.py
@@ -0,0 +1,13 @@
+from llenarClases import *
+from tomarOrden import *
+
+def main():
+    tomarOrden();
+
+    listaOrdenes = llenarClases()
+
+    #Mandar lista ordenes a cada grafica y a la tabla
+            
+if __name__ == "__main__":
+    main()
+    sys.exit()
