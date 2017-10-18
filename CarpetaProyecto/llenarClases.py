00847a6301a89be3f581e87ad80b2eb32e6d01eb
Arturo Lopez <huascar.arturo96@gmail.com>
2017-10-18 16:21:27 -0700

    Actuaizando para cada mensaje.

--- CarpetaProyecto/llenarClases.py
+++ CarpetaProyecto/llenarClases.py
@@ -4,23 +4,24 @@ from Orden import *
 
 def llenarClases():
     with open("ordenesJSON.json") as json_file:
-         data = json.load(json_file)
+         data_json = json.load(json_file)
 
-    ordenes=[]
-    
-    datetime=data["datetime"]
-    request=data["request_id"]
-    for i in range(len(data["orden"])):
-        o= Orden(data["orden"][i]["part_id"],
-                 data["orden"][i]["type"],
-                 data["orden"][i]["meat"],
-                 data["orden"][i]["quantity"],
-                 data["orden"][i]["ingredients"])
-        ordenes.append(o)    
-    answer=data["answer"]
+    for data in data_json:
+        ordenes=[]
 
-    oc= OrdenCompleta(datetime,request,ordenes,answer)
-    return oc
+        datetime=data["datetime"]
+        request=data["request_id"]
+        for i in range(len(data["orden"])):
+            o= Orden(data["orden"][i]["part_id"],
+                     data["orden"][i]["type"],
+                     data["orden"][i]["meat"],
+                     data["orden"][i]["quantity"],
+                     data["orden"][i]["ingredients"])
+            ordenes.append(o)    
+        answer=data["answer"]
+
+        oc= OrdenCompleta(datetime,request,ordenes,answer)
+        return oc
 
 ##ordenes= llenarClases()
 ##print(ordenes)
