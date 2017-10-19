d6dd43efec1fb35c671692b9036e03da8de97e68
Arturo Lopez <huascar.arturo96@gmail.com>
2017-10-18 17:38:30 -0700

    Update llenarClases.py

--- CarpetaProyecto/llenarClases.py
+++ CarpetaProyecto/llenarClases.py
@@ -5,24 +5,24 @@ from Orden import *
 def llenarClases():
     listaOrdenes=[]
     with open("ordenesJSON.json") as json_file:
-        data_json = json.load(json_file)
+        data = json.load(json_file)
+        
+        ordenes=[]
 
-        for data in data_json:
-            ordenes=[]
-
-            datetime=data["datetime"]
-            request=data["request_id"]
-            for i in range(len(data["orden"])):
-                o= Orden(data["orden"][i]["part_id"],
+        datetime=data["datetime"]
+        request=data["request_id"]
+        for i in range(len(data["orden"])):
+            o= Orden(data["orden"][i]["part_id"],
                          data["orden"][i]["type"],
                          data["orden"][i]["meat"],
                          data["orden"][i]["quantity"],
                          data["orden"][i]["ingredients"])
-                ordenes.append(o)    
-            answer=data["answer"]
+            ordenes.append(o)    
+        answer=data["answer"]
 
-            oc= OrdenCompleta(datetime,request,ordenes,answer)
-            listaOrdenes.append(oc)
+        oc= OrdenCompleta(datetime,request,ordenes,answer)
+        listaOrdenes.append(oc)
+        
     return listaOrdenes 
 
 ##ordenes= llenarClases()
