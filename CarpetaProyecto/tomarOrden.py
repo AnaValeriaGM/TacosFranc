53952309e5e22830826c9ff7a3b4553839380dcd
Arturo Lopez <huascar.arturo96@gmail.com>
2017-11-20 19:40:35 -0800

    Update tomarOrden.py

--- CarpetaProyecto/tomarOrden.py
+++ CarpetaProyecto/tomarOrden.py
@@ -1,6 +1,6 @@
 import boto3
 
-def tomarOrden():
+def leerSQS():
 	sqs = boto3.client('sqs')
 
 	response = sqs.receive_message(
@@ -27,7 +27,8 @@ def escribirJson(ordenes):
 	archivo.close()
 
 def comenzar():
-	escribirJson(tomarOrden())
+	mensajes= leerSQS()
+	escribirJson(mensajes)
 
 #for r in recibos:
    # response = sqs.delete_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/292274580527/cc406_team4',ReceiptHandle=r)
